4118d96b73bc56d1281625700cb1476ea9422430
ahmedmustafa48 <ahmedcodes48@gmail.com>
2025-05-30 19:17:50 +0500

    Implement responsive design in Header, Repost, and Threads components; utilize media queries to adjust layout based on screen size.

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,31 +1,71 @@
-import { Stack } from "@mui/material";
+import { Grid, Stack, useMediaQuery } from "@mui/material";
 import React from "react";
 import Navbar from "./Navbar";
 import { IoMenu } from "react-icons/io5";
 const Header = () => {
+  const _700 = useMediaQuery("(min-width=700px)");
   return (
     <div>
-      <Stack
-        flexDirection={"row"}
-        height={52}
-        justifyContent={"space-around"}
-        alignItems={"center"}
-        position={"sticky"}
-        top={0}
-        py={1}
-      >
-        <img src="/Threads-logo-black-bg.webp" alt="" width={60} height={40} />
+      {_700 ? (
         <Stack
-          justifyContent={"center"}
-          width={"550px"}
-          bgcolor={"aliceblue"}
-          zIndex={2}
-          height={96}
+          flexDirection={"row"}
+          height={52}
+          justifyContent={"space-around"}
+          alignItems={"center"}
+          position={"sticky"}
+          top={0}
+          py={1}
         >
-          <Navbar />
+          <img
+            src="/Threads-logo-black-bg.webp"
+            alt=""
+            width={60}
+            height={40}
+          />
+          <Stack
+            justifyContent={"center"}
+            width={"550px"}
+            bgcolor={"aliceblue"}
+            zIndex={2}
+            height={96}
+          >
+            <Navbar />
+          </Stack>
+          <IoMenu size={32} class="image-icon" color="gray" />
         </Stack>
-        <IoMenu size={32} class="image-icon" />
-      </Stack>
+      ) : (
+        <>
+          <Stack
+            position={"fixed"}
+            bottom={0}
+            justifyContent={"center"}
+            width={"100%"}
+            height={52}
+            p={1}
+            bgcolor={"aliceblue"}
+            zIndex={2}
+          >
+            <Navbar />
+          </Stack>
+          <Grid
+            container
+            height={60}
+            justifyContent={"flex-end"}
+            alignItems={"center"}
+            p={1}
+          >
+            <Grid item xs={6} md={4}>
+              <img
+                src="/Threads-logo-white-bg.png"
+                alt="logo"
+                width={60}
+                height={35}
+              />
+            </Grid>
+            <IoMenu size={32} class="image-icon" color="gray" />
+          </Grid>
+        </>
+      )}
     </div>
   );
 };
